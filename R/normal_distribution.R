0301fcdd539574b7a89736a8349d0c424ac3b69c
Narinder Singh <nss470@gmail.com>
2024-05-18 18:26:04 -0500

    input validation and remove redundant function call

--- R/normal_distribution.R
+++ R/normal_distribution.R
@@ -20,6 +20,11 @@ plot_norm_dist <- function(m = 0, sd = 1, n = 1e6,
    #' This function plots the normal distribution density curve with 
    #' empirical rule annotations
    
+   # Input Validation
+   if (!is.numeric(m) || !is.numeric(sd) || sd <= 0) {
+      stop("Invalid input: 'm' and 'sd' must be numeric, and 'sd' must be positive.")
+   }
+   
    # simulate sampling from normal distribution
    set.seed(1)
    norm_dist <- rnorm(n = n, mean = m, sd = sd)
@@ -56,32 +61,37 @@ plot_norm_dist <- function(m = 0, sd = 1, n = 1e6,
    if (annotations) {
       # local function to find value of y for specific sd on x
       find_y_kde <- function(nsd) kde$y[kde$x == min(kde$x[kde$x >= m+sd*nsd])]
+      # find kde for 1-3 sd 
+      # (multiplied by small factor to allow vertical lines go over kde)
+      y_kde_1 <- find_y_kde(1) * 1.3
+      y_kde_2 <- find_y_kde(2) * 2.5
+      y_kde_3 <- find_y_kde(3) * 10
       
       # # add vertical line segments for mean and sd
       segments(x0 = ticks, y0 = 0, x1 = ticks, 
                y1 = c(0, 
-                      find_y_kde(3)*10, find_y_kde(2)*2.5, find_y_kde(1)*1.3,
+                      y_kde_3, y_kde_2, y_kde_1,
                       max(kde$y),
-                      find_y_kde(1)*1.3, find_y_kde(2)*2.5, find_y_kde(3)*10, 
+                      y_kde_1, y_kde_2, y_kde_3, 
                       0), 
                lty = 2, col = "gray")
       
       # add annotation arrows
       arrows(x0 = sort(ticks[ticks < m]),
              x1 = sort(ticks[ticks > m], decreasing = T),
-             y0 = c(0, find_y_kde(3)*10, find_y_kde(2)*2.5, find_y_kde(1)*1.3),
-             y1 = c(0, find_y_kde(3)*10, find_y_kde(2)*2.5, find_y_kde(1)*1.3),
+             y0 = c(0, y_kde_3, y_kde_2, y_kde_1),
+             y1 = c(0, y_kde_3, y_kde_2, y_kde_1),
              col = c(NA, "red", "blue", "green"),
              code = 3, lty = 1)
 
       # # add text annotation boxes
       rect(xleft = m-0.45*sd,
-           ybottom = c(find_y_kde(1)*1.3, find_y_kde(2)*2.5, find_y_kde(3)*10)  - 0.015/sd,
+           ybottom = c(y_kde_1, y_kde_2, y_kde_3)  - 0.015/sd,
            xright = m+0.45*sd,
-           ytop = c(find_y_kde(1)*1.3, find_y_kde(2)*2.5, find_y_kde(3)*10) + 0.015/sd,
+           ytop = c(y_kde_1, y_kde_2, y_kde_3) + 0.015/sd,
            border = c("green", "blue", "red"), col = "white")
       text(x = m,
-           y = c(find_y_kde(1)*1.3, find_y_kde(2)*2.5, find_y_kde(3)*10),
+           y = c(y_kde_1, y_kde_2, y_kde_3),
            labels = c("68%", "95%", "99.7%"),
            cex = 1.25, font = 2, col = "black")
    }
